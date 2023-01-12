71f2c183ea9e789b5e1d34821b85db65ca2fdb6b
Tamilia <apfellfcnn@gmail.com>
2023-01-12 10:39:46 +0600

    Added search

--- source/blog/forms.py
+++ source/blog/forms.py
@@ -6,3 +6,7 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'body', 'status']
+
+
+class SimpleSearchForm(forms.Form):
+    search = forms.CharField(max_length=50, required=False, label='Search')
--- source/blog/views/post_views.py
+++ source/blog/views/post_views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import get_object_or_404, redirect, reverse
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, View
-
-from blog.forms import PostForm
+from django.utils.http import urlencode
+from blog.forms import PostForm, SimpleSearchForm
 from blog.models import Post
+from django.db.models import Q
 
 
 class PostIndexView(ListView):
@@ -11,6 +12,33 @@ class PostIndexView(ListView):
     model = Post
     paginate_by = 3
     paginate_orphans = 1
+
+    def get(self, request, *args, **kwargs):
+        self.form = self.get_search_form()
+        self.search_value = self.get_search_value()
+        return super().get(request, *args, **kwargs)
+
+    def get_search_form(self):
+        return SimpleSearchForm(self.request.GET)
+
+    def get_search_value(self):
+        if self.form.is_valid():
+            return self.form.cleaned_data['search']
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        if self.search_value:
+            queryset = queryset.filter(Q(title__icontains=self.search_value) |
+                                       Q(body__icontains=self.search_value))
+        return queryset
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(object_list=object_list, **kwargs)
+        context['form'] = self.form
+        if self.search_value:
+            context['query'] = urlencode({'search': self.search_value})
+            context['search'] = self.search_value
+        return context
 
 
 class PostView(DetailView):
